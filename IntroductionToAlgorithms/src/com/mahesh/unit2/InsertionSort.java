e791ef789bcc5fd44e5bd6cbf9956dee5bda67f5
Mahesh <maheshneogi@yahoo.com>
2018-08-26 13:28:56 +0530

    Insertion Sort

--- IntroductionToAlgorithms/src/com/mahesh/unit2/InsertionSort.java
+++ IntroductionToAlgorithms/src/com/mahesh/unit2/InsertionSort.java
@@ -1,10 +1,27 @@
 package com.mahesh.unit2;
 
+import java.util.Arrays;
+
 public class InsertionSort {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//coment
+	private static void sort(int[] inputArray) {
+		for (int j = 1; j < inputArray.length; j++) {
+			int key = inputArray[j];
+			int i = j - 1;
+			while (i > -1 && inputArray[i] > key) {
+				inputArray[i + 1] = inputArray[i];
+				i = i - 1;
+			}
+			inputArray[i + 1] = key;
+		}
+		Arrays.stream(inputArray).forEach(item -> {
+			System.out.print(item);
+			System.out.print("\t");
+		});
 	}
 
+	public static void main(String[] args) {
+		int[] inputArray = new int[] { 5, 2, 4, 6, 1, 3 };
+		sort(inputArray);
+	}
 }
